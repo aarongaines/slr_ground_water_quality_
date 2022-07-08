49809c7452519b888def3389bf9d0cab8818079d
aarongaines <72536103+aarongaines@users.noreply.github.com>
2022-07-08 16:11:55 -0700

    modify contaminant parameters added vocs, conts10, conts11

--- slr_pkg/para.py
+++ slr_pkg/para.py
@@ -1,15 +1,42 @@
 # Module file for SLR_Groud_Water_Quality repository parameters.
 
-# List of contaminant paramaters.
-contaminants = [
-'BZ', 
-'BZME', 
-'DBCP', 
-'EBZ', 
-'EDB',  
-'MTBE', 
-'PCE', 
-'TCE', 
-'TCPR123', 
-'XYLENES', 
-]
+# List of VOCs contaminants.
+vocs = [
+    'BZ', 
+    'BZME', 
+    'DBCP', 
+    'EBZ', 
+    'EDB',  
+    'MTBE', 
+    'PCE', 
+    'TCE', 
+    'TCPR123', 
+    'XYLENES', 
+    ]
+
+conts10 = [
+    'TCPR123',
+    'CD',
+    'PB',
+    'EDB',
+    'PCE',
+    'TCE',
+    'BZ',
+    'BZME',
+    'EBZ',
+    'XYLENES',
+    ]
+
+conts11 = [
+    'TCPR123',
+    'CD',
+    'PB',
+    'EDB',
+    'PCE',
+    'TCE',
+    'BZ',
+    'BZME',
+    'EBZ',
+    'XYLENES',
+    'MTBE',
+    ]
